5d9c48c77b9158829411ec82204d7fb297c69615
Elaheh <elah0048@stud.kea.dk>
2022-11-13 21:18:20 +0100

    added style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,29 @@
 import React from 'react'
 import './Navbar.css'
-import { FaFacebookF,FaLinkedinIn, FaTwitter } from "react-icons/fa";
+import { FaFacebookF,FaLinkedinIn, FaTwitter, FaBars } from "react-icons/fa";
 
 
 const Navbar = () => {
   return (
-      <nav>
-       
-       
-        <ul className='nav-social'>
+      <nav >
+        <div className='container'>
+          <div className='nav-logo'>
+            <button><FaBars/></button>
+            <img src='../logo192.png'></img>
+          </div>
+          <div className='nav-links'>
+            <ul className='list'>
+              <li><a href='/'>Home</a></li>
+              <li><a href='/'>Projects</a></li>
+              <li><a href='/'>About Us</a></li>
+              <li><a href='/'>Contact</a></li>
+           </ul>
+          </div>
+          <ul className='nav-social' >
             <li><a href='/'><FaFacebookF/></a></li>
             <li><a href='/'><FaLinkedinIn/></a></li>
             <li><a href='/'><FaTwitter/></a></li>
-        </ul>
-        <ul className='nav-links'>
-           <li><a href='/'>Home</a></li>
-           <li><a href='/'>Projects</a></li>
-           <li><a href='/'>About Us</a></li>
-           <li><a href='/'>Contact</a></li>
-        </ul>
-        <div className='nav-logo'>
-            <img src='../logo192.png'></img>
+          </ul>
         </div>
       </nav>
   )
